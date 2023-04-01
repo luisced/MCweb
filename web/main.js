b39f6a696ecd5943d4234ff29bf383a0639510bc
luisced <luis.cedillo.maldonado@gmail.com>
2023-04-01 15:14:53 -0600

    added zip

---
+++ web/main.js
@@ -0,0 +1,24 @@
+document.querySelectorAll(".copy-link").forEach((copyLinkParent) => {
+	const inputField = copyLinkParent.querySelector(".copy-link-input");
+	const copyButton = copyLinkParent.querySelector(".copy-link-button");
+	const text = inputField.value;
+
+	inputField.addEventListener("focus", () => inputField.select());
+
+	copyButton.addEventListener("click", () => {
+		inputField.select();
+		navigator.clipboard.writeText(text);
+
+		inputField.value = "Copied!";
+		setTimeout(() => (inputField.value = text), 2000);
+	});
+});
+
+function downloadMods() {
+	var link = document.createElement("a");
+	link.href = "assets/mods.zip";
+	link.download = "mods.zip";
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
+}
